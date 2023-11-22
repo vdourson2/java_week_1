596454f8f9b480d9f7a92dddca25adbe8b86d543
Virginie Dourson <virginiedourson@gmail.com>
2023-11-22 11:53:07 +0100

    Challenge_1

--- src/java_week_1/Hello2.java
+++ src/java_week_1/Hello2.java
@@ -3,8 +3,19 @@ package java_week_1;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello2");
-
+		char t1 = 'e';
+		byte t2 = 120;
+		short t3 = 1546;
+		int t4 = 1_000_000;
+		long t5 = 2_000_000_000_000l;
+		boolean t6 = true;
+		float t7 = 7.43f;
+		double t8 = 7.33e10;
+		
+		System.out.println("Char type : \n char : " + t1);
+		System.out.println("Integer types : \n byte : " + t2 + "\n short : " + t3 + "\n int : " + t4 + "\n long : " + t5);
+		System.out.println("Boolean type : \n boolean : " + t6);
+		System.out.println("Floating-point types : \n float : " + t7 + "\n double : " + t8);
 	}
 
 }
