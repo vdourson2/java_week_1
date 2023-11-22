98c646d73fa3d35595747927891eff1fa98b1987
Virginie Dourson <virginiedourson@gmail.com>
2023-11-22 10:55:37 +0100

    Start repo

---
+++ src/java_week_1/Hello1.java
@@ -0,0 +1,9 @@
+package java_week_1;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello1");
+	}
+
+}
---
+++ src/java_week_1/Hello2.java
@@ -0,0 +1,10 @@
+package java_week_1;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello2");
+
+	}
+
+}
