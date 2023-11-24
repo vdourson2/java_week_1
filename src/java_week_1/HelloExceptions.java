14e1c26461f2aa74b1e02868fcc25e0556358aab
Virginie Dourson <virginiedourson@gmail.com>
2023-11-24 11:57:23 +0100

    End  Challenge_4

--- src/java_week_1/HelloExceptions.java
+++ src/java_week_1/HelloExceptions.java
@@ -6,20 +6,28 @@ public class HelloExceptions {
 
 	public static void main(String[] args) {
 		Integer[] randomArray = new Integer[10];
+		//Complete randomArray with random values from 0 to 10
 		for(byte i=0;i<10;i++) {
 			randomArray[i] = randomZeroToTen();
 		}
 		System.out.println(Arrays.toString(randomArray));
+		
 		int index = 0;
 		Integer[] dividedArray = new Integer[10];
-		for(Integer el:randomArray) {
-			if (index == 0) {
-				dividedArray[0] = randomArray[0]/randomArray[0]; 
+		try {
+			for(Integer el:randomArray) {
+				if (index == 0) {
+					dividedArray[0] = randomArray[0]/randomArray[0]; 
+				}
+				else{
+					dividedArray[index] = randomArray[index]/randomArray[index-1];
+				}
+				index++;
 			}
-			else{
-				dividedArray[index] = randomArray[index]/randomArray[index-1];
-			}
-			index++;
+		}
+		catch (Exception e) {
+			//System.out.println("You cannot divide a number by o - " + e);
+			throw new IllegalArgumentException("You cannot divide a number by o - " + e);
 		}
 		System.out.println(Arrays.toString(dividedArray));
 		
